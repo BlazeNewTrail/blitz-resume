26273ed83b7591dc8e71ad92fdb14f007f1fb50d
Sebastian Bresin <sebastian.bresin@gmail.com>
2020-05-04 19:50:17 +0200

    add sidebar + themeswitcher

---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    class="theme-switcher slds-button slds-icon-waffle_container"
+    title="Description of the icon when needed"
+    @click="toggleDarkMode"
+  >
+    <span class="slds-icon-waffle">
+      <span class="slds-r1"></span>
+      <span class="slds-r2"></span>
+      <span class="slds-r3"></span>
+      <span class="slds-r4"></span>
+      <span class="slds-r5"></span>
+      <span class="slds-r6"></span>
+      <span class="slds-r7"></span>
+      <span class="slds-r8"></span>
+      <span class="slds-r9"></span>
+    </span>
+    <span class="slds-assistive-text">Open App Launcher</span>
+  </button>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    darkMode: true
+  }),
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+
+      document.documentElement.classList.toggle("dark-mode");
+    }
+  }
+};
+</script>
+
+<style>
+.theme-switcher {
+  display: block;
+  position: fixed !important;
+  left: 0;
+  bottom: 0;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,126 +1,23 @@
 <template>
-  <div class="layout">
-    <header class="slds-global-header_container">
-      <a
-        href="javascript:void(0);"
-        class="slds-assistive-text slds-assistive-text_focus"
-      >Skip to Navigation</a>
-      <a
-        href="javascript:void(0);"
-        class="slds-assistive-text slds-assistive-text_focus"
-      >Skip to Main Content</a>
-      <div class="slds-global-header slds-grid slds-grid_align-spread">
-        <div class="slds-global-header__item">
-          <div class="slds-global-header__logo">
-            <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+  <div class="slds-scope site-page-wrapper">
+    <div>
+      <sidebar />
+      <theme-switcher />
+      <main
+        class="site-main slds-order_1"
+        role="main"
+        id="site-main-content"
+        tabindex="-1"
+      >
+      <!-- slds-brand-band slds-brand-band_cover slds-brand-band_medium  -->
+        <div
+          class="slds-template_default"
+        >
+          <div class="slds-template__container">
+            <slot />
           </div>
         </div>
-        <div class="slds-global-header__item">
-          <ul class="slds-global-actions">
-            <li class="slds-global-actions__item">
-              <div class="slds-dropdown-trigger slds-dropdown-trigger_click">
-                <button
-                  class="slds-button slds-button_icon slds-button_icon slds-button_icon-container slds-button_icon-small slds-global-actions__task slds-global-actions__item-action"
-                  aria-haspopup="true"
-                  title="Global Actions"
-                >
-                  <svg
-                    class="slds-button__icon"
-                    aria-hidden="true"
-                  >
-                    <use
-                      xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#add"
-                    />
-                  </svg>
-                  <span class="slds-assistive-text">Global Actions</span>
-                </button>
-              </div>
-            </li>
-            <li class="slds-global-actions__item">
-              <div class="slds-dropdown-trigger slds-dropdown-trigger_click">
-                <button
-                  class="slds-button slds-button_icon slds-button_icon slds-button_icon-container slds-button_icon-small slds-global-actions__help slds-global-actions__item-action"
-                  aria-haspopup="true"
-                  title="Help and Training"
-                >
-                  <svg
-                    class="slds-button__icon slds-global-header__icon"
-                    aria-hidden="true"
-                  >
-                    <use
-                      xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#question"
-                    />
-                  </svg>
-                  <span class="slds-assistive-text">Help and Training</span>
-                </button>
-              </div>
-            </li>
-            <li class="slds-global-actions__item">
-              <div class="slds-dropdown-trigger slds-dropdown-trigger_click">
-                <button
-                  class="slds-button slds-button_icon slds-button_icon slds-button_icon-container slds-button_icon-small slds-global-actions__setup slds-global-actions__item-action"
-                  aria-haspopup="true"
-                  title="Setup"
-                >
-                  <svg
-                    class="slds-button__icon slds-global-header__icon"
-                    aria-hidden="true"
-                  >
-                    <use
-                      xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#setup"
-                    />
-                  </svg>
-                  <span class="slds-assistive-text">Setup</span>
-                </button>
-              </div>
-            </li>
-            <li class="slds-global-actions__item">
-              <div class="slds-dropdown-trigger slds-dropdown-trigger_click">
-                <button
-                  class="slds-button slds-button_icon slds-button_icon slds-button_icon-container slds-button_icon-small slds-global-actions__notifications slds-global-actions__item-action"
-                  title="no new notifications"
-                  aria-live="assertive"
-                  aria-atomic="true"
-                >
-                  <svg
-                    class="slds-button__icon slds-global-header__icon"
-                    aria-hidden="true"
-                  >
-                    <use
-                      xlink:href="@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg#notification"
-                    />
-                  </svg>
-                  <span class="slds-assistive-text">no new notifications</span>
-                </button>
-                <span
-                  aria-hidden="true"
-                  class="slds-notification-badge"
-                >0</span>
-              </div>
-            </li>
-            <li class="slds-global-actions__item">
-              <div class="slds-dropdown-trigger slds-dropdown-trigger_click">
-                <button
-                  class="slds-button slds-global-actions__avatar slds-global-actions__item-action"
-                  title="person name"
-                  aria-haspopup="true"
-                >
-                  <span class="slds-avatar slds-avatar_circle slds-avatar_medium">
-                    <img
-                      alt="Person name"
-                      src="@salesforce-ux/design-system/assets/images/avatar2.jpg"
-                      title="User avatar"
-                    />
-                  </span>
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
-    <div class="content slds-template_default">
-    <slot />
+      </main>
     </div>
   </div>
 </template>
@@ -132,3 +29,15 @@ query {
   }
 }
 </static-query>
+
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
+
+export default {
+  components: {
+    Sidebar,
+    ThemeSwitcher
+  }
+};
+</script>
