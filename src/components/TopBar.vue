c6e1b4c8d7e4e1865e09037b59bc5578b86bc408
Sebastian Bresin <sebastian.bresin@gmail.com>
2020-06-17 16:25:48 +0200

    feat: automatic navLinks

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -21,7 +21,6 @@
                 <span class="slds-r8"></span>
                 <span class="slds-r9"></span>
               </span>
-              <span class="slds-assistive-text">Can be removed</span>
             </button>
           </div>
           <span
@@ -33,88 +32,40 @@
           </span>
         </div>
       </div>
-      <scroll-active-nav class="slds-context-bar__secondary slds-show_medium">
-        <ul class="slds-grid">
-          <li data-navkey="about" class="slds-context-bar__item slds-is-active">
-            <a
-              v-scroll-to="'#about'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="About"
+      <scroll-active-nav>
+        <nav
+          slot-scope="{ activeKey, sections }"
+          class="slds-context-bar__secondary slds-show_medium"
+        >
+          <ul class="slds-grid">
+            <li
+              v-for="section in sections"
+              :key="section.id"
+              :class="{ 'slds-is-active': activeKey === section.id }"
+              class="slds-context-bar__item"
             >
-              <span class="slds-truncate" title="About">About</span>
-            </a>
-          </li>
-          <li data-navkey="work" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#work'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Work"
-            >
-              <span class="slds-truncate" title="Work">Work</span>
-            </a>
-          </li>
-          <li data-navkey="skills" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#skills'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Skills"
-            >
-              <span class="slds-truncate" title="Skills">Skills</span>
-            </a>
-          </li>
-          <li data-navkey="projects" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#projects'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Projects"
-            >
-              <span class="slds-truncate" title="Projects">Projects</span>
-            </a>
-          </li>
-          <li data-navkey="education" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#education'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Education"
-            >
-              <span class="slds-truncate" title="Education">Education</span>
-            </a>
-          </li>
-          <li data-navkey="awards" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#awards'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Awards"
-            >
-              <span class="slds-truncate" title="Awards">Certifications</span>
-            </a>
-          </li>
-          <li data-navkey="talks" class="slds-context-bar__item">
-            <a
-              v-scroll-to="'#talks'"
-              href="#"
-              class="slds-context-bar__label-action"
-              title="Talks"
-            >
-              <span class="slds-truncate" title="Talks">Talks</span>
-            </a>
-          </li>
-        </ul>
-        <!-- </nav> -->
+              <a
+                v-scroll-to="`#${section.id}`"
+                href="#"
+                class="slds-context-bar__label-action"
+              >
+                <span class="slds-truncate">
+                  {{ section.label }}
+                </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </scroll-active-nav>
-      <theme-switcher class="slds-col_bump-left slds-align-middle slds-p-right_medium" />
+      <theme-switcher
+        class="slds-col_bump-left slds-align-middle slds-p-right_medium"
+      />
     </div>
   </header>
 </template>
 
 <script>
-import ScrollActiveNav from '@/components/scroll-active-nav';
+import ScrollActiveNav from '@/components/ScrollActiveNav';
 import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
 
 export default {
